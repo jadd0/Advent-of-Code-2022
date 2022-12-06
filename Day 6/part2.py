83341fa513ccfbe51acb78ad9e592b54160f7990
jadd0 <102069567+jadd0@users.noreply.github.com>
2022-12-06 09:14:19 +0000

    part 2 done

---
+++ Day 6/part2.py
@@ -0,0 +1,16 @@
+lines = open('text.txt').read().strip().splitlines()
+
+val = 0
+
+for line in lines:
+  stack = []
+  for i in range(len(line)):
+    if len(stack) < 14:
+      stack.append(line[i])
+    else:
+      if (len(set(stack)) == len(stack)):
+        val = i
+        break
+      stack.pop(0)
+      stack.append(line[i])
+  print(val)
